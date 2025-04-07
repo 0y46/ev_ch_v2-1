034d6dde0378d3bdeb96fe866076566d3e5ba904
q2b2 <aa2003961@qu.edu.qa>
2025-04-07 23:22:32 +0300

    update

--- start_monitor.sh
+++ start_monitor.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Wait for desktop environment to fully load
-
-
 # Activate virtual environment
-source /home/qeeri/ev_ch/ev_ch_v2/venv_system/bin/activate
+source /home/qeeri/Desktop/ev_ch_v2/venv_system/bin/activate
 
 # Navigate to the project directory
-cd /home/qeeri/ev_ch/ev_ch_v2
+cd /home/qeeri/Desktop/ev_ch_v2
 
 # Start the application in fullscreen mode
-python main.py --real-data --udp-ip 127.0.0.1 --udp-port 8888 --fullscreen
+python main.py --real-data --udp-ip 127.0.0.1 --udp-port 8888 --fullscreen >> /home/qeeri/Desktop/ev_ch_v2/app.log 2>&1
